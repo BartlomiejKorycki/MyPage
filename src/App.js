7e240bf5db88c088d5c50ad4f941def60c6095e4
Bart <korysiu@gmail.com>
2019-10-15 15:24:48 +0200

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './styles/App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
