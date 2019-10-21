08a6c6cbf67eb8068768ae1c77642057d3ea48ef
Bart <korysiu@gmail.com>
2019-10-21 17:06:17 +0200

    footer finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,22 +3,27 @@ import React from 'react';
 class Footer extends React.Component {
     render() {
         return(
-            <div className="footer-container">
-                <artcile>
-                    Jeżeli masz jakieś pytania to pisz :)
+            <div className="footer-container row">
+                <artcile className="col-md-6 text-center contact-art"> 
+                    <p className="cont-ent">If you need more informations or have any questions, contact me please.</p>
                 </artcile>
-                <button>
-                    Contact
-                </button>
-                <a className="Linkedin" href="linkedin.com">
-                    <i class="fab fa-linkedin"></i>
-                </a>
-                <a className="Github" href="github.com">
-                    <i class="fab fa-github-square"></i>
-                </a>
-                <div className="footer-copyright text-center">
+                <div className="col-md-2  text-center contactb">
+                    <a className=" btn btn-default contact-btn" href="/">
+                        Contact
+                    </a>
+                </div>
+                <div className="col-md-4 text-center social">
+                    <p className="social-info">Links</p>
+                    <a className="Linkedin" href="linkedin.com">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                    <a className="Github" href="github.com">
+                        <i class="fab fa-github-square"></i>
+                    </a>
+                </div>
+                <div className="footer-copyright text-center container-fluid">
                     <container>
-                    &copy; {new Date().getFullYear()} Bartlomiej Korycki
+                        &copy; {new Date().getFullYear()} Bartlomiej Korycki
                     </container>
                 </div>
             </div>
