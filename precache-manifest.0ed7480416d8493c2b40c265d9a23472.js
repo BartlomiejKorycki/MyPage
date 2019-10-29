ff558d30c028982f077a92dd201af6c4ddc0b543
Bart <korysiu@gmail.com>
2019-10-29 02:10:57 +0100

    Updates

---
+++ precache-manifest.0ed7480416d8493c2b40c265d9a23472.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0927e426a6c08dd456d0d69b0c6d068",
+    "url": "/MyPage/index.html"
+  },
+  {
+    "revision": "251930c25c2408715995",
+    "url": "/MyPage/static/css/main.b9789e1a.chunk.css"
+  },
+  {
+    "revision": "6d5c9b7f7a6088cf3f47",
+    "url": "/MyPage/static/js/2.998e4fe0.chunk.js"
+  },
+  {
+    "revision": "251930c25c2408715995",
+    "url": "/MyPage/static/js/main.6a29a92c.chunk.js"
+  },
+  {
+    "revision": "dc55f7f4758b28d55ffe",
+    "url": "/MyPage/static/js/runtime-main.2574571a.js"
+  },
+  {
+    "revision": "dff6d5098712f75c6ebb855b8b3ac94f",
+    "url": "/MyPage/static/media/Mountains.dff6d509.jpg"
+  },
+  {
+    "revision": "be650a65af983c6ab65e6422f1011544",
+    "url": "/MyPage/static/media/UKFLAG.be650a65.jpg"
+  },
+  {
+    "revision": "dcdc4a8129af115f089391049a37bad0",
+    "url": "/MyPage/static/media/bedoka.dcdc4a81.png"
+  },
+  {
+    "revision": "977e3fd0f430205e51e65488c11fdddc",
+    "url": "/MyPage/static/media/topography.977e3fd0.png"
+  }
+]);
